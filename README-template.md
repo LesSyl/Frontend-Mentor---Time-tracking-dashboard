a54d9040224783f1d30a6f64b773531f1079e154
LesSyl <127938956+LesSyl@users.noreply.github.com>
2024-11-27 12:51:46 +0100

    created time tracking

---
+++ README-template.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+## Table of contents
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+Desktop
+
+![](dist/img/desktop.jpeg)
+
+Mobile
+
+![](dist/img/mobile.jpeg)
+
+
+### Links
+
+- Repository URL:[GitHub](https://github.com/LesSyl/Newsletter-sign-up-form-with-success-message)
+- Solution URL: [Frontendmentor]()
+- Live Site URL: [Live](https://lessyl.github.io/Newsletter-sign-up-form-with-success-message/)
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Mobile-first workflow
+- Sass
+- BEM
+- Flexbox
+- JS
+- kit
+- JSON
+
+### What I learned
+
+In this challenge, I worked on creating responsive websites, which gave me a better understanding of techniques for adapting a website's appearance to different device resolutions. I also learned how to add my own fonts using @font-face, which gives me more freedom to customise the aesthetics of the site. The main issues I have mastered are methods to prevent the page from reloading, so that the user can use the site smoothly. In addition, I have explored the principles of correct form validation, which allows for effective protection of user data and better management of errors when entering information.
+
+## Author
+
+- Frontend Mentor - [@LesSyl](https://www.frontendmentor.io/profile/LesSyl)
+- gitHub - [@LesSyl](https://github.com/LesSyl)
---
+++ src/js/main.js
@@ -0,0 +1,68 @@
+const menuItems = document.querySelectorAll('.name-section__time-frames-btn')
+const nameCard = document.querySelectorAll('.activity-container__title-h2')
+const hrs = document.querySelectorAll('.activity-container__time-hrs')
+const duration = document.querySelectorAll('.activity-container__time-last')
+
+const URL = '/data.json'
+
+async function fetchData() {
+	try {
+		const res = await fetch(URL)
+		if (!res.ok) {
+			console.log('Oops! Something went wrong.')
+			throw new Error(response.statusText)
+		}
+		const data = await res.json()
+		return data
+	} catch {
+		console.error('error')
+	}
+}
+
+async function time(name) {
+	const data = await fetchData()
+	if (!data) return
+
+	data.forEach((item, i) => {
+		nameCard[i].textContent = item.title
+		hrs[i].textContent = `${item.timeframes[name].current}hrs`
+
+		const num = `${item.timeframes[name].previous}hrs`
+		console.log()
+		if (name === 'daily') {
+			duration[i].textContent = 'Yesterday - ' + num
+		} else if (name === 'weekly') {
+			duration[i].textContent = 'Last Week - ' + num
+		} else if (name === 'monthly') {
+			duration[i].textContent = 'Last Month - ' + num
+		}
+	})
+}
+
+
+const activeLink = selectedLink => {
+	menuItems.forEach(link => {
+		if (link === selectedLink) {
+			link.classList.add('name-section--active')
+		} else {
+			link.classList.remove('name-section--active')
+		}
+	})
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	const link = document.querySelector('.name-section__time-frames-btn[data-default]')
+	const defaultTime = link.textContent.toLowerCase()
+	time(defaultTime)
+	activeLink(link)
+	// console.log(defaultTime);
+})
+
+menuItems.forEach(link => {
+	link.addEventListener('click', e => {
+		e.preventDefault()
+		const selectedTimeFrame = link.textContent.toLowerCase()
+		time(selectedTimeFrame)
+		activeLink(link)
+	})
+})
