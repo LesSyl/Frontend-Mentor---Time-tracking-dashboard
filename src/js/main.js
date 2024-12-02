73669f8f97e7b21eea3441658adc3f0b85407df0
LesSyl <127938956+LesSyl@users.noreply.github.com>
2024-12-02 08:51:38 +0100

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ const nameCard = document.querySelectorAll('.activity-container__title-h2')
 const hrs = document.querySelectorAll('.activity-container__time-hrs')
 const duration = document.querySelectorAll('.activity-container__time-last')
 
+
+
 async function fetchData() {
-	const URL = '../../../time-tracking-dashboard-main/data.json'
+	const URL = "../../../time-tracking-dashboard-main/data.json"
 	try {
 		const response = await fetch(URL)
 		if (!response.ok) {
